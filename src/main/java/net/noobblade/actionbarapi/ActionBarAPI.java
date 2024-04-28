43075ef4ff6e230d2860799b504a05d80110893f
Noobblade <noobbladediscord@gmail.com>
2024-04-28 05:24:48 +0000

    Initial Commit

---
+++ src/main/java/net/noobblade/actionbarapi/ActionBarAPI.java
@@ -0,0 +1,17 @@
+package net.noobblade.actionbarapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ActionBarAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
