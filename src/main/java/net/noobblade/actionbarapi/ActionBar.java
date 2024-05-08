6b5f47e34d10bf92d8b0284554685792edfb08ca
Noobblade <noobbladediscord@gmail.com>
2024-05-08 08:39:44 +0000

    Final optimization

--- src/main/java/net/noobblade/actionbarapi/ActionBar.java
+++ src/main/java/net/noobblade/actionbarapi/ActionBar.java
@@ -6,7 +6,6 @@ import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.ChatColor;
 
 public class ActionBar {
 
--- src/main/java/net/noobblade/actionbarapi/ActionBarAPI.java
+++ src/main/java/net/noobblade/actionbarapi/ActionBarAPI.java
@@ -1,17 +1,20 @@
 package net.noobblade.actionbarapi;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ActionBarAPI extends JavaPlugin {
+    public static Plugin plugin;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        Server server =  getServer();
+        getLogger().info(ChatColor.AQUA + plugin.getName() + ChatColor.GREEN + " has been enabled." );
 
     }
+    //removed onDisable() because nothing to do with that
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
